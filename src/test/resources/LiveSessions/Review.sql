e895734b7718fd8b530b4ed86b98c4ba96c3425e
arthur-rule <arthur.rule@cydeo.com>
2024-07-29 00:09:18 +0300

    07/28/2024

--- src/test/resources/LiveSessions/Review.sql
+++ src/test/resources/LiveSessions/Review.sql
@@ -123,18 +123,70 @@ ORDER BY SALARY DESC ;
 
 -- IQ 2 --> Write a query to print first_name and salary for all employees in the Employee table who earn a salary larger than 3000. Sort your results in ascending order of the last 3 characters in the employees first_name if two or more employees have first_names ending with same 3 characters, then sort them by highest salary in desc
 
-    SELECT  FIRST_NAME, SALARY
+    SELECT  FIRST_NAME, SALARY , LOWER(SUBSTR(FIRST_NAME,-3)) AS LAST_THREE
     FROM EMPLOYEES
+    WHERE SALARY>3000
+    ORDER BY LAST_THREE, SALARY DESC ;
 
 -- TASK 10- 1 - How many city we have in each country from locations table
 
+    SELECT * FROM LOCATIONS;
+
+    SELECT COUNTRY_ID FROM LOCATIONS;
+
+    SELECT DISTINCT  COUNTRY_ID FROM LOCATIONS;
+
+    SELECT   COUNTRY_ID FROM LOCATIONS
+    GROUP BY COUNTRY_ID;
+
+    SELECT   COUNTRY_ID, COUNT(*) FROM LOCATIONS
+    GROUP BY COUNTRY_ID;
+
+    SELECT   COUNTRY_ID, COUNT(CITY) FROM LOCATIONS
+    GROUP BY COUNTRY_ID;
+
+    SELECT * FROM EMPLOYEES;
+    SELECT COUNT(*) FROM EMPLOYEES;
+    SELECT COUNT(DEPARTMENT_ID) FROM EMPLOYEES;
+
 -- TASK 10- 2  - order them based on city count in desc
+
+    SELECT   COUNTRY_ID, COUNT(*) AS CITY_COUNT FROM LOCATIONS
+    GROUP BY COUNTRY_ID
+    ORDER BY CITY_COUNT DESC ;
 
 -- TASK 10- 3 - Filter result where country id starts with C
 
+    --OPT1
+    SELECT   COUNTRY_ID, COUNT(*) AS CITY_COUNT
+    FROM LOCATIONS
+    WHERE COUNTRY_ID LIKE 'C%'
+    GROUP BY COUNTRY_ID
+    ORDER BY CITY_COUNT DESC ;
+
+    --OPT2
+    SELECT   COUNTRY_ID, COUNT(*) AS CITY_COUNT
+    FROM LOCATIONS
+    GROUP BY COUNTRY_ID
+    HAVING COUNTRY_ID LIKE 'C%'
+    ORDER BY CITY_COUNT DESC ;
+
 -- TASK 10- 4 -  Display country id if city count is bigger than 1
 
+    SELECT   COUNTRY_ID, COUNT(CITY) AS CITY_COUNT
+    FROM LOCATIONS
+    GROUP BY COUNTRY_ID
+    HAVING COUNT(CITY)>1
+    ORDER BY CITY_COUNT DESC ;
+
 -- TASK 10- 5 - Display country id that starts with C
+
+    SELECT   COUNTRY_ID, COUNT(CITY) AS CITY_COUNT
+    FROM LOCATIONS
+    WHERE COUNTRY_ID LIKE 'C%'
+    GROUP BY COUNTRY_ID
+    HAVING COUNT(CITY)>1
+    ORDER BY CITY_COUNT DESC ;
 
 -- TASK 11 -1  How many employees is working for EACH department
 
@@ -152,17 +204,73 @@ ORDER BY SALARY DESC ;
 
 -- TASK 14 - Display all information who is getting 11th highest salary
 
--- TASK 15 - Display all first_name and  related department_name
+-----------------------------------------------------------------------------------------
+    --SQL COMMANDS--
+    --DQL
+        --SELECT
+    --DML
+        --INSERT
+        --UPDATE
+        --DELETE
+    --DDL
+        --CREATE
+        --ALTER
+        --DROP
+        --TRUNCATE
+    --DCL
+    --TCL
 
--- TASK 16 -1 Display all first_name, last name and department_name including the  employee without
+---------------------------------------------------------------------------------------
+    --JOINS
+        --INNER
+        --OUTER
+            --> LEFT OUTER
+            --> RIGHT OUTER
+            --> FULL OUTER
+        --SELF
 
--- TASK 16 -2 Display all first_name and department_name including the  department without employee
+------------------------------------------------------------------------------------------
+-- TASK 15 - Display all first_name, last mane and  related department_name
 
--- TASK 16 -3 Display all first_name and department_name including the department without employee
+    SELECT FIRST_NAME, LAST_NAME, DEPARTMENT_ID FROM EMPLOYEES;
+
+    SELECT DEPARTMENT_ID, DEPARTMENT_NAME FROM DEPARTMENTS;
+
+    SELECT FIRST_NAME, LAST_NAME, DEPARTMENT_NAME
+    FROM EMPLOYEES E
+            JOIN DEPARTMENTS D ON E.DEPARTMENT_ID = D.DEPARTMENT_ID;
+
+
+-- TASK 16 -1 Display all first_name, last name and department_name including the  employee without department id
+
+    SELECT FIRST_NAME, LAST_NAME, DEPARTMENT_NAME
+    FROM EMPLOYEES E
+        LEFT JOIN DEPARTMENTS D
+                            ON E.DEPARTMENT_ID=D.DEPARTMENT_ID;
+
+
+-- TASK 16 -2 Display all first_name and department_name including the  department without employee using right join
+
+    SELECT FIRST_NAME, LAST_NAME, DEPARTMENT_NAME
+    FROM EMPLOYEES E
+             RIGHT JOIN DEPARTMENTS D
+                             ON E.DEPARTMENT_ID=D.DEPARTMENT_ID;
+
+-- TASK 16 -3 Display all first_name and department_name including the department without employee using left join
+
+    SELECT FIRST_NAME, LAST_NAME, DEPARTMENT_NAME
+    FROM DEPARTMENTS D
+             LEFT JOIN  EMPLOYEES E
+                        ON E.DEPARTMENT_ID=D.DEPARTMENT_ID;
 
 -- TASK 16 -4 Display all first_name and department_name including the  employee without department
 
 -- TASK 16 -5 Display all firstname and department name including the department without employees and also employees without the department
+
+    SELECT FIRST_NAME, LAST_NAME, DEPARTMENT_NAME
+    FROM DEPARTMENTS D
+             FULL JOIN  EMPLOYEES E
+                        ON E.DEPARTMENT_ID=D.DEPARTMENT_ID;
 
 -- TASK 17 -1 How many employees working for each department (use department name)
 
@@ -170,12 +278,50 @@ ORDER BY SALARY DESC ;
 
 -- TASK 18 - Display all employees with their manager first_name and last_names
 
+    SELECT EMPLOYEE_ID, FIRST_NAME, LAST_NAME, MANAGER_ID
+    FROM EMPLOYEES; -->all employees -->107
+
+    SELECT MANAGER_ID, FIRST_NAME, LAST_NAME
+    FROM EMPLOYEES;
+
+    SELECT * FROM EMPLOYEES WORKERS
+                JOIN EMPLOYEES MANAGERS
+                        ON WORKERS.MANAGER_ID=MANAGERS.EMPLOYEE_ID;
+
+    SELECT WORKERS.FIRST_NAME, WORKERS.LAST_NAME, WORKERS.MANAGER_ID,
+           MANAGERS.EMPLOYEE_ID, MANAGERS.FIRST_NAME, MANAGERS.LAST_NAME
+    FROM EMPLOYEES WORKERS
+                      LEFT JOIN EMPLOYEES MANAGERS
+                           ON WORKERS.MANAGER_ID=MANAGERS.EMPLOYEE_ID;
+
 -- TASK 19 - Display all EMPLOYEES who is manager
 
 -- TASK 20 - Display all EMPLOYEES who is not manager
 --First find all manager IDs
 
 -- IQ3 - Given the Employee table, write a SQL query that finds out employees who earn more than their managers.
+
+    SELECT WORKERS.DEPARTMENT_ID, WORKERS.FIRST_NAME, WORKERS.LAST_NAME, WORKERS.SALARY, MANAGERS.SALARY,
+           MANAGERS.FIRST_NAME, MANAGERS.LAST_NAME
+    FROM EMPLOYEES WORKERS
+                    JOIN EMPLOYEES MANAGERS
+                        ON WORKERS.MANAGER_ID=MANAGERS.EMPLOYEE_ID
+            WHERE WORKERS.SALARY > MANAGERS.SALARY;
+
+    SELECT WORKERS.DEPARTMENT_ID
+    FROM EMPLOYEES WORKERS
+             JOIN EMPLOYEES MANAGERS
+                  ON WORKERS.MANAGER_ID=MANAGERS.EMPLOYEE_ID
+    WHERE WORKERS.SALARY > MANAGERS.SALARY;
+
+
+
+SELECT  DEPARTMENT_NAME FROM DEPARTMENTS
+WHERE DEPARTMENT_ID=( SELECT DISTINCT WORKERS.DEPARTMENT_ID
+                      FROM EMPLOYEES WORKERS
+                               JOIN EMPLOYEES MANAGERS
+                                    ON WORKERS.MANAGER_ID=MANAGERS.EMPLOYEE_ID
+                      WHERE WORKERS.SALARY > MANAGERS.SALARY );
 
 -- IQ4 - Given the Employee table, write a SQL query that finds out amount of 3th highest salary
 
